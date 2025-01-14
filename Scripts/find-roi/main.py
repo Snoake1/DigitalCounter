eb3036a97ef15c7ac7fdcae8f4c9dae4f488f65c
hurma_nn <99395782+hestiates@users.noreply.github.com>
2025-01-14 16:27:27 +0300

    Update main.py

--- Scripts/find-roi/main.py
+++ Scripts/find-roi/main.py
@@ -2,19 +2,13 @@ import cv2
 import numpy as np
 
 img = cv2.imread('test_image/test1.jpg')
-#ресайз по х и у
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-hsv = cv2.blur(hsv, (5, 5))  # наложение стрмной маски чбшной
+hsv = cv2.blur(hsv, (5, 5))
 mask = cv2.inRange(hsv, (89, 124, 73), (255, 255, 255))
-#hsv = cv2.cvtColor(mask, cv2.COLOR_BGR2HSV)
 lower_blue = np.array([124, 123, 138])
 
 
 upper_blue = np.array([150, 150, 150])
-#тута менять фильтр
-#mask = cv2.inRange(hsv, lower_blue, upper_blue)
-#imgray = cv2.cvtColor(mask, 127, 255, 0)
-#ret, thresh = cv2.threshold(imgray, 127, 255, 0)
 
 contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 counturs = sorted(contours, key=cv2.contourArea, reverse=True)
@@ -26,7 +20,4 @@ for contour in counturs:
         key = cv2.waitKey(1)
         if key == 27:
             break
-    #cv2.imshow("Mask", mask)
-   # cv2.imshow("ret",ret)
-    #cv2.imshow("blur",blurred_frame)
 cv2.destroyAllWindows()
