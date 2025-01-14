1e6dba15c970556a406a7d3952c9619535846171
Pavel Bogatyrev <pavelbogatirov@mail.ru>
2025-01-14 12:37:16 +0300

    add some scripts

---
+++ Scripts/barcodeReader.py
@@ -0,0 +1,53 @@
+# Importing library 
+import cv2
+import imutils
+from pyzbar.pyzbar import decode
+import numpy as np
+
+
+# Make one method to decode the barcode
+def barcode_reader(image):
+    # read the image in numpy array using cv2
+    img = cv2.imread(image)
+
+    # Decode the barcode image 
+    detected_barcodes = decode(img)
+
+    # If not detected then print the message 
+    if not detected_barcodes:
+        print(f"Barcode Not Detected or your barcode is blank/corrupted! File name: {image}")
+    else:
+
+        # Traverse through all the detected barcodes in image
+        for (i, barcode) in enumerate(detected_barcodes):
+
+            # Locate the barcode position in image 
+            (x, y, w, h) = barcode.rect
+            # Определяем координаты для обрезки изображения
+            top_left = (x - 100, y - 100)
+            bottom_right = (x + w + 100, y + h + 100)
+
+            cv2.imwrite(f"ProcessResults/barcoder{i}{image.split("/")[-1]}", img[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]])
+            
+            # Put the rectangle in image using  
+            # cv2 to highlight the barcode 
+            rect = cv2.rectangle(img, (x - 100, y - 100),
+                                (x + w + 100, y + h + 100),
+                                (255, 0, 0), 2)
+
+            if barcode.data != "":
+                # Print the barcode data
+                print(barcode.data)
+                print(barcode.type)
+            else:
+                pass    #TODO find digits
+
+    cv2.imwrite(f"ProcessResults/barcoder{image.split("/")[-1]}", img)
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    # convert_filenames_to_lowercase("")
+    # Take the image from user
+    image = "../Dataset/IMG_20240924_074029.jpg"
+    barcode_reader(image)
---
+++ Scripts/processImages.py
@@ -0,0 +1,43 @@
+import os
+import argparse
+import importlib
+
+
+def main():
+    # Настройка парсера аргументов
+    parser = argparse.ArgumentParser(description='Обработка файлов в каталоге с заданной функцией.')
+    parser.add_argument('file_name', type=str, help='Имя файла, где находится функция (без .py)')
+    parser.add_argument('function_name', type=str, help='Имя функции для вызова')
+
+    args = parser.parse_args()
+
+    try:
+        # Импортируем файл с функциями
+        file_processor = importlib.import_module(args.file_name)
+
+        # Получаем ссылку на функцию по имени
+        function = getattr(file_processor, args.function_name, None)
+
+        if function:
+            # Перебираем файлы в указанном каталоге и вызываем функцию
+            directory = "../Dataset"  # Убедитесь, что этот путь существует
+            for filename in os.listdir(directory):
+                file_path = os.path.join(directory, filename)  # Полный путь к файлу
+                if filename.endswith(".jpg") or filename.endswith(".png"):
+                    # Вызываем функцию с полным путем к файлу
+                    try:
+                        function(file_path)
+                    except Exception as e:
+                        print(f"Произошла ошибка: {e}")
+                        continue
+                else:
+                    continue
+        else:
+            print(f"Функция '{args.function_name}' не найдена в модуле '{args.file_name}'.")
+    except ImportError:
+        print(f"Не удалось импортировать модуль '{args.file_name}'. Проверьте имя файла и путь.")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
+if __name__ == "__main__":
+    main()
