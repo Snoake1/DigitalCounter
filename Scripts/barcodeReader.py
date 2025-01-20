f8d03b78c34ef5c22a10ec05219e3d42f6313dc8
Pavel Bogatyrev <pavelbogatirov@mail.ru>
2025-01-20 12:30:46 +0300

    write main

--- Scripts/barcodeReader.py
+++ Scripts/barcodeReader.py
@@ -2,21 +2,17 @@
 import cv2
 import imutils
 from pyzbar.pyzbar import decode
-import numpy as np
 
 
 # Make one method to decode the barcode
-def barcode_reader(image):
-    name = image.split("\\")[-1]
-    # read the image in numpy array using cv2
-    img = cv2.imread(image)
+def barcode_reader(img):
 
     # Decode the barcode image 
     detected_barcodes = decode(img)
 
     # If not detected then print the message 
     if not detected_barcodes:
-        print(f"Barcode Not Detected or your barcode is blank/corrupted! File name: {name}")
+        return f"Barcode Not Detected or your barcode is blank/corrupted!"
     else:
 
         # Traverse through all the detected barcodes in image
@@ -25,30 +21,28 @@ def barcode_reader(image):
             # Locate the barcode position in image 
             (x, y, w, h) = barcode.rect
             # Определяем координаты для обрезки изображения
-            top_left = (x - 100, y - 100)
-            bottom_right = (x + w + 100, y + h + 100)
+            top_left = (x+10, y+10)
+            bottom_right = (x + w + 10, y + h + 10)
 
-            cv2.imwrite(f"ProcessResults/barcoder{i}{name}", img[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]])
+            #cv2.imwrite(f"ProcessResults/barcoder{i}{name}", img[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]])
             
             # Put the rectangle in image using  
             # cv2 to highlight the barcode 
-            rect = cv2.rectangle(img, (x - 100, y - 100),
-                                (x + w + 100, y + h + 100),
+            rect = cv2.rectangle(img, (x - 10, y - 10),
+                                (x + w + 10, y + h + 10),
                                 (255, 0, 0), 2)
 
             if barcode.data != "":
                 # Print the barcode data
-                print(barcode.data)
-                print(barcode.type)
-            else:
-                pass    #TODO find digits
+                return barcode.data
 
-    cv2.imwrite(f"ProcessResults/barcoder{name}", img)
-    cv2.destroyAllWindows()
+    #cv2.imwrite(f"ProcessResults/barcoder{name}", img)
+    #cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
     # convert_filenames_to_lowercase("")
     # Take the image from user
     image = "../Dataset/IMG_20240924_074029.jpg"
+    image = cv2.imread(image)
     barcode_reader(image)
---
+++ Scripts/main.py
@@ -0,0 +1,34 @@
+import argparse
+import cv2
+from barcodeReader import barcode_reader
+from ultralytics import YOLO
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Получение данных счетчика по изображение')
+    parser.add_argument('file_name', type=str, help='Имя файла, где находится изображение')
+
+    args = parser.parse_args()
+    name = args.file_name.split("\\")[-1]
+    # read the image in numpy array using cv2
+    img = cv2.imread(name)
+
+    model = YOLO("../yolo.pt")
+    res = model.predict(img)[0]
+
+    num_roi = img
+    disp_roi = img
+    for i, cls in enumerate(res.boxes.cls):
+        x1, y1, x2, y2 = res.boxes.xyxy[i].int()
+        if cls == 0:
+            disp_roi = img[y1:y2, x1:x2]
+        elif cls == 1:
+            num_roi = img[y1:y2, x1:x2]
+
+    value = 0  #TODO Получать значение на дисплее
+    barcode_value = barcode_reader(num_roi)
+    if barcode_value.isdigit():
+        print(f"Заводской номер: {barcode_value}")
+    else:
+        #TODO Отображать номер, полученный OCR
+        print(f"Не удалось прочитать заводской номер")
