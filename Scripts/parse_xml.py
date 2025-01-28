230eeb6f70fd491b2ad089af9f927708c7717a35
Pavel Bogatyrev <pavelbogatirov@mail.ru>
2025-01-28 09:43:45 +0300

    change parse_xml

--- Scripts/parse_xml.py
+++ Scripts/parse_xml.py
@@ -3,14 +3,19 @@ import xml.etree.ElementTree as ET
 tree = ET.parse("annotations.xml")
 images = tree.findall("image")
 
-with open("labels_train.csv", "w+") as f:
-    for i in images:
-        name = i.attrib['name']
-        if i.attrib['subset'] != 'Train':
-            continue
-        for p in i:
-            if p.attrib['label'] != 'Display':
+
+def get_csv_from_xml(attr='Display'):
+    with open("prodid_train.csv", "w+", encoding="utf-8") as f:
+        for i in images:
+            name = i.attrib['name']
+            if i.attrib['subset'] != 'Train':
                 continue
-            text = p[0].text
-            if text != "10000":
-                f.write(f"{name}, {text}\n")
+            for p in i:
+                if p.attrib['label'] != attr:
+                    continue
+                text = p[0].text
+                if text != "10000":
+                    f.write(f"{name}, {text}\n")
+
+
+get_csv_from_xml("ProdId")
